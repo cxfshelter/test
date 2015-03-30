6de63f49862eaaa22e6d5164bb7ad952eb0e6b91
zz <743314921@qq.com>
2015-03-30 20:52:04 +0800

    add service

--- gen/com/example/test2/R.java
+++ gen/com/example/test2/R.java
@@ -8,6 +8,9 @@
 package com.example.test2;
 
 public final class R {
+    public static final class array {
+        public static final int least_than_oneMin=0x7f060000;
+    }
     public static final class attr {
     }
     public static final class dimen {
@@ -173,32 +176,32 @@ public final class R {
         public static final int title_shadow=0x7f020099;
     }
     public static final class id {
-        public static final int action_settings=0x7f070019;
-        public static final int actionbarLayout=0x7f070004;
-        public static final int atDescTextView=0x7f070010;
-        public static final int atLayout=0x7f07000b;
-        public static final int atTextView=0x7f07000f;
-        public static final int backImageView=0x7f070013;
-        public static final int btnStart=0x7f070000;
-        public static final int checkedImageView=0x7f070018;
-        public static final int closeImageView=0x7f070008;
-        public static final int divider=0x7f070009;
-        public static final int gridView=0x7f070015;
-        public static final int hScrollView=0x7f07000d;
-        public static final int imageRemoveBtn=0x7f070012;
-        public static final int imageView=0x7f070011;
-        public static final int imagesLinearLayout=0x7f07000e;
-        public static final int logoImageView=0x7f070016;
-        public static final int mainRelLayout=0x7f070005;
-        public static final int nameTextView=0x7f070017;
-        public static final int okImageView=0x7f070014;
-        public static final int shareBtn=0x7f070003;
-        public static final int textCounterTextView=0x7f07000c;
-        public static final int textEditText=0x7f07000a;
-        public static final int textFinalTime=0x7f070002;
-        public static final int textTime=0x7f070001;
-        public static final int titleEditText=0x7f070007;
-        public static final int titleLayout=0x7f070006;
+        public static final int action_settings=0x7f080019;
+        public static final int actionbarLayout=0x7f080004;
+        public static final int atDescTextView=0x7f080010;
+        public static final int atLayout=0x7f08000b;
+        public static final int atTextView=0x7f08000f;
+        public static final int backImageView=0x7f080013;
+        public static final int btnStart=0x7f080000;
+        public static final int checkedImageView=0x7f080018;
+        public static final int closeImageView=0x7f080008;
+        public static final int divider=0x7f080009;
+        public static final int gridView=0x7f080015;
+        public static final int hScrollView=0x7f08000d;
+        public static final int imageRemoveBtn=0x7f080012;
+        public static final int imageView=0x7f080011;
+        public static final int imagesLinearLayout=0x7f08000e;
+        public static final int logoImageView=0x7f080016;
+        public static final int mainRelLayout=0x7f080005;
+        public static final int nameTextView=0x7f080017;
+        public static final int okImageView=0x7f080014;
+        public static final int shareBtn=0x7f080003;
+        public static final int textCounterTextView=0x7f08000c;
+        public static final int textEditText=0x7f08000a;
+        public static final int textFinalTime=0x7f080002;
+        public static final int textTime=0x7f080001;
+        public static final int titleEditText=0x7f080007;
+        public static final int titleLayout=0x7f080006;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -211,7 +214,7 @@ public final class R {
         public static final int skyblue_share_platform_list_item=0x7f030007;
     }
     public static final class menu {
-        public static final int main=0x7f060000;
+        public static final int main=0x7f070000;
     }
     public static final class string {
         public static final int action_settings=0x7f050058;
