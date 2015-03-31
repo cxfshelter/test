d69533eedc1421a0a0fdb2e64b08457da3a5fa4f
Wrq <263454190@qq.com>
2015-04-01 00:10:30 +0800

    增加成绩显示UI，修改记录成绩工具类

--- src/com/example/util/RecordUtil.java
+++ src/com/example/util/RecordUtil.java
@@ -26,6 +26,25 @@ public class RecordUtil {
      */
     final static String BEST_SCORE_KEY = "best_score_key";
 
+
+    /**
+     * 返回上一次成绩
+     * @param context 上下文
+     * @return 上一次成绩
+     */
+    public static long getLastScore(Context context) {
+        return getScore(LAST_SCORE_KEY, context);
+    }
+
+    /**
+     * 返回最好成绩
+     * @param context 上下文
+     * @return 最好成绩
+     */
+    public static long getBestScore(Context context) {
+        return getScore(BEST_SCORE_KEY, context);
+    }
+
     /**
      * 根据不同的KEY获取相应成绩
      * @param key 不同的成绩对应的KEY值
